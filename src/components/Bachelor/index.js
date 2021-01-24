6bb720d2a22516d9418aa3b6c874169b5409a53a
Logan Cool <logan.cool@stitchfix.com>
2021-01-23 17:00:59 -0800

    write some test code

---
+++ src/components/Bachelor/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Bachelor = () => {
+  const funs = 'test';
+
+  return (
+    <div>
+      {'test'}
+    </div>
+  )
+}
+
+export default Bachelor;
