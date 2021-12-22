fdf18ee12fc7d8f25deeae180473d62dec179d51
Logan Cool <logan.cool@stitchfix.com>
2021-12-22 10:18:21 -0800

    dont use node_env check

--- src/services/fetchEpisode.js
+++ src/services/fetchEpisode.js
@@ -1,19 +1,23 @@
+const TVDB_API_HOST =
+	process.env.NODE_ENV === "development"
+		? ""
+		: process.env.REACT_APP_TVDB_API_HOST;
+
 async function fetchEpisode(token, episode) {
-  try {
-    console.log(`trying to fetch ${episode} episode`);
-    const response = await fetch(`/v4/series/${episode}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      },
-    });
+	try {
+		console.log(`trying to fetch ${episode} episode`);
+		const response = await fetch(`${TVDB_API_HOST}/v4/series/${episode}`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
 
-    return response.json();
-
-  } catch (err) {
-    console.log(`failure getting ${episode} episode`);
-    throw new Error(err);
-  }
+		return response.json();
+	} catch (err) {
+		console.log(`failure getting ${episode} episode`);
+		throw new Error(err);
+	}
 }
 
 export default fetchEpisode;
